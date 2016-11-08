2dd1e4f2bd025ce42cb4462fafc79df6910376ba
Naoki <hnron.reg@gmail.com>
2016-11-09 04:55:14 +0900

    [fix] init-common.sh: apt install add lvm2

--- scripts/init-common.sh
+++ scripts/init-common.sh
@@ -4,7 +4,7 @@ sudo apt update
 
 sudo apt -y dist-upgrade
 
-sudo apt -y install aptitude build-essential git ntp ntpdate openssh-server python-dev sudo bridge-utils vlan tmux vim curl
+sudo apt -y install aptitude build-essential git ntp ntpdate openssh-server python-dev sudo bridge-utils vlan lvm2 tmux vim curl
 
 sudo sh -c "echo 'bonding' >> /etc/modules"
 sudo sh -c " echo '8021q' >> /etc/modules"
