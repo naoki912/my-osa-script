cbfc3696d2e38563b175cfd3a2fed3379fcbc3ad
Naoki <hnron.reg@gmail.com>
2016-11-09 04:46:55 +0900

    [fix] scripts/init-common.sh: modules sudo fix

--- scripts/init-common.sh
+++ scripts/init-common.sh
@@ -6,5 +6,5 @@ sudo apt -y dist-upgrade
 
 sudo apt -y install aptitude build-essential git ntp ntpdate openssh-server python-dev sudo bridge-utils vlan tmux vim curl
 
-sudo echo 'bonding' >> /etc/modules
-sudo echo '8021q' >> /etc/modules
+sudo sh -c "echo 'bonding' >> /etc/modules"
+sudo sh -c " echo '8021q' >> /etc/modules"
