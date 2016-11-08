1b65bbef565e09994bc6781d7def99cc25fb30f4
Naoki <hnron.reg@gmail.com>
2016-11-09 00:34:27 +0900

    [fix] openstack-ansible repository: openstack -> naoki912

--- scripts/init-osa-controller.sh
+++ scripts/init-osa-controller.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
-git clone -b stable/newton https://github.com/openstack/openstack-ansible /opt/openstack-ansible
+git clone -b myosa/stable/newton https://github.com/naoki912/openstack-ansible /opt/openstack-ansible
 
 cp -r /opt/openstack-ansible/etc/openstack_deploy /etc/openstack_deploy
 
-cp ../files/haproxy_config.yml /opt/openstack-ansible/playbooks/vars/configs/haproxy_config.yml
 cp ../files/openstack_user_config.yml /etc/openstack_deploy/openstack_user_config.yml
 
 cd /opt/openstack-ansible
